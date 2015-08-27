e247f11642567b3889f99ad1dc015cd928beed4b
William Decker <wdw1062@gaintjoe.edu>
2015-08-27 17:06:59 -0500

    making it look pretty-er -er

--- test.c
+++ test.c
@@ -2,30 +2,29 @@
 
 #include<stdio.h>
 int main(int argc, char** argv){
-    // you might consider to use scanf() for user input
    int low = 0, up = 0;
-   char input[8];
+   char input[8]; //total numbers possible for input
 
     while(1){
         printf("please q and hit Enter to exit \n");
 
         printf("Please enter the lower limit. Integer only. : ");
-	low = atoi(fgets(input, 8, stdin));
+	low = atoi(fgets(input, 8, stdin));  // sets first user input to low
 	printf("Got %d for low\n", low);
         
         printf("please input the upper limit. : ");
-        up = atoi(fgets(input,8,stdin));
+        up = atoi(fgets(input,8,stdin));  // sets second user input to up
 	printf("Got %d for high\n", up);
         
-        int count = 0;
+        int count = 0;  // resets count every time its run
         
-        for(low; low < (up + 1); ++low){
+        for(low; low < (up + 1); ++low){ // logic
           if (low%7 != 0){
             count += low;
           }
         }
         
-        printf("the total sum of numbers between lower and upper that cannot be divided exactly by 7 is %d \n", count);
+        printf("the total sum of numbers between lower and upper /n that cannot be divided exactly by 7 is %d \n", count);
     }
     return 0;// do not change this line
 }
